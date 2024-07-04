7a731665f58db63b6f6dd828e99c6d811e5e3a9a
Hamed Bahram <hamed.bahram@gmail.com>
2024-07-04 09:49:55 -0400

    Update home title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>API Rate Limiting</h1>
+        <h1 className='text-3xl font-bold'>Rate limiting</h1>
+        <p className='text-sm text-muted-foreground'>
+          Using NextJs Middleware and Vercel KV Store
+        </p>
       </div>
     </section>
   )
